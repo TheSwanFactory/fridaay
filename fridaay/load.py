cc0789f2fa81ccfa4561615e956d5e0cfbe19f51
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-06-30 18:58:25 -0700

    remove cache

--- fridaay/load.py
+++ fridaay/load.py
@@ -2,8 +2,7 @@ import os,sys,yaml
 PIPE_FOLDER="pipes"
 
 def read_yaml(filename, folder=PIPE_FOLDER):
-    print(os.getcwd())
-    path = os.path.join(os.getcwd(),PIPE_FOLDER, filename)
+    path = os.path.join(PIPE_FOLDER, filename)
     with open(path, 'r') as file:
         yml = yaml.safe_load(file)
         return yml
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,8 +1,6 @@
 from .conftest import *
 
 def test_yaml():
-    print(os.getcwd())
-
     yml = read_yaml(TEST_FILE)
     assert yml
     #assert 'init' in yml
