2d8d7449e687e39969fa37591e93b6ef6bd0609b
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-06-30 21:34:34 -0700

    read_schema

--- fridaay/load.py
+++ fridaay/load.py
@@ -1,8 +1,14 @@
 import os,sys,yaml
 PIPE_FOLDER="pipes"
+DAD_FOLDER="fridaay/dad"
 
 def read_yaml(filename, folder=PIPE_FOLDER):
     path = os.path.join(PIPE_FOLDER, filename)
     with open(path, 'r') as file:
         yml = yaml.safe_load(file)
         return yml
+
+def read_schema(action):
+    filename = f'dad-{action}'
+    yml = read_yaml(filename, DAD_FOLDER)
+    return yml
