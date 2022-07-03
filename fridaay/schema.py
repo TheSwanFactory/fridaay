6f6c3531707e17f7ff103df1ac28af3a83ae03a1
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-02 22:41:52 -0700

    default values

--- fridaay/schema.py
+++ fridaay/schema.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import json
 from .constants import *
 
 class Schema:
@@ -7,11 +8,18 @@ class Schema:
         self.dad = dad
         self.keys = self.sanitize(dad.keys())
         self.struct = namedtuple(self.act, self.keys)
+        self.struct.__doc__ = json.dumps(dad)
 
     def sanitize(self, keys):
         return [k if k != K_FROM else K_FKEY for k in keys]
 
+    def default(self, action):
+        for k in self.keys:
+            if k not in action: action[k] = False
+        return action
+
     def parse(self, action):
         if K_FROM in action: action[F_KEY] = action.pop(K_FROM)
-        tuple = self.struct(**action)
+        defaulted = self.default(action)
+        tuple = self.struct(**defaulted)
         return tuple
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,7 +3,7 @@ from .conftest import *
 
 DICT = {
 "a": "alpha",
-"b": "beta"
+"b": "beta*"
 }
 
 @pytest.fixture
@@ -18,3 +18,15 @@ def test_parse(schema):
     p = schema.parse(DICT)
     assert p
     assert p.a == DICT["a"]
+
+def test_optional(schema):
+    DICT.pop("a")
+    p = schema.parse(DICT)
+    assert p
+
+def test_mandatory():
+    with pytest.raises(Exception) as e:
+        DICT.pop("b")
+        print(DICT)
+        p = schema.parse(DICT)
+    print(e)
