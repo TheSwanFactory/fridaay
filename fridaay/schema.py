1f525c458514362d69d1ffe28d0fce33ead47d41
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-02 22:53:16 -0700

    self.sanitize(dad)

--- fridaay/schema.py
+++ fridaay/schema.py
@@ -5,16 +5,22 @@ from .constants import *
 class Schema:
     def __init__(self, act, dad):
         self.act = act
-        self.dad = dad
-        self.keys = self.sanitize(dad.keys())
+        self.dad = {}
+        self.keys = []
+        self.optional = []
+        self.sanitize(dad)
         self.struct = namedtuple(self.act, self.keys)
         self.struct.__doc__ = json.dumps(dad)
 
-    def sanitize(self, keys):
-        return [k if k != K_FROM else K_FKEY for k in keys]
+    def sanitize(self, dad):
+        for k, v in dad.items():
+            key = k if k != K_FROM else K_FKEY
+            self.dad[key] = v
+            self.keys.append(key)
+            if v[-1] != '*': self.optional.append(key)
 
     def default(self, action):
-        for k in self.keys:
+        for k in self.optional:
             if k not in action: action[k] = False
         return action
 
