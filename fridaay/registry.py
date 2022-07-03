f1af623dfb028035e6817fee6d4036eab9083a7c
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-03 15:23:55 -0700

    fail reg.load

--- fridaay/registry.py
+++ fridaay/registry.py
@@ -5,6 +5,7 @@ from .schema import Schema
 class Registry:
     def __init__(self, folder=DAD_FOLDER):
         self.schemas = {}
+        self.modules = {}
         self.add_folder(folder)
 
     def add_dad(self, name, yml):
@@ -25,4 +26,7 @@ class Registry:
         return obj
 
     def load(self, imports):
+        for key, value in imports.items():
+            mod = __import__(value, globals=None, locals=None, fromlist=True)
+            self.modules[key] = mod
         return True
