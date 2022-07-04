85a4df3f896d7fcf135536c92acadabceb9c4087
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-03 18:24:26 -0700

    test_pandas

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -1,25 +1,6 @@
-import pytest
-from .conftest import *
-from dad_sql_pandas import *
 import pandas as pd
+from dad_sql_pandas import *
 
-@pytest.fixture
-def pipe():
-    yml = load_yaml(TEST_FILE)
-    r = Registry()
-    p = Pipe(r, yml)
-    return p
-
-def test_pipe(pipe):
-    assert pipe
-
-def test_compile(pipe):
-    asms = pipe.compile()
-    assert pipe.vars['SAPIENT']
-    print(asms)
-    assert len(asms) == 2
-    a0 = asms[0]
-    assert a0.do == 'sql.load'
-    a1 = asms[1]
-    print(a1)
-    assert a1.from_key == a0.id
+def test_pandas():
+    df = pd.DataFrame()
+    assert df.empty
