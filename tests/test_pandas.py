222331b92f81933673d2896fab1d9abf010442cd
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-03 18:36:04 -0700

    PDATA

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from dad_sql_pandas import *
 
+PDATA = {
+    "columns": ["A", "B"],
+    "data": [[1,2],[11,12],[21,22]]
+}
+
 def test_pandas():
     df = pd.DataFrame()
     assert df.empty
 
 def test_create():
     columns = ['A','B']
-    data = [[1,2],[11,12],[21,22]]
-    df = pd.DataFrame(data=data, columns=columns)
+    df = pd.DataFrame(**PDATA)
     assert not df.empty
