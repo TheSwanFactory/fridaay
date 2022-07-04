382f00434d3a3a24c2c1ce7e2cd4adfa278c3a47
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-03 20:38:50 -0700

    test_date

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,5 +1,6 @@
 import pytest
 from .conftest import *
+import datetime
 
 @pytest.fixture
 def yml(): return load_yaml(TEST_FILE)
@@ -16,3 +17,14 @@ def test_yamls():
 def test_init(yml):
     action = yml['fridaay']
     assert TEST_DO == action['do']
+
+def test_date(yml):
+    action = yml['test_data']
+    data = action['data']
+    row = data[0]
+    assert row[4] < datetime.date(2022, 1, 1)
+
+    action = yml['demo_pets']
+    data = action['where_all']
+    row = data[0]
+    assert row[2] == datetime.date(2022, 1, 1)
