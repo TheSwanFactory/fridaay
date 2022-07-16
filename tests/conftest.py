0a1da3e88bd8bbf5b5dcf919aeac7cdaed48e622
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-15 17:46:13 -0700

    test PKG_NAME

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-    '.', './fridaay', '..', '../fridaay'
-)))
-
-from fridaay import *
 PKG_NAME='fridaay'
 TEST_FILE="demo_pets"
 TEST_DO='core.init'
 TEST_ID='test_data'
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+    '.', f'./{PKG_NAME}', '..', f'../{PKG_NAME}'
+)))
+
+from fridaay import *
